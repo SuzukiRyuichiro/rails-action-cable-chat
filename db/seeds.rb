d662b46bb3c100a564b4d69e952fc8475fb5a647
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2022-11-28 09:37:03 +0100

    messages can be shown but not in real time

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Message.destroy_all
+Chatroom.destroy_all
+User.destroy_all
+
+chatroom = Chatroom.create(name: "general")
+scooter = User.create(email: "mail@mail.com", nickname: "Scooter", password: "123123")
+boris = User.create(email: "mail@mail.org", nickname: "Boris", password: "123123")
+
+Message.create(content: "Hello", user: scooter, chatroom: chatroom)
+Message.create(content: "Hola", user: boris, chatroom: chatroom)
